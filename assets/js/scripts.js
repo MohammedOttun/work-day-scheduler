24ceccd51966232694a39bf4bf913d20beea65ee
MohammedOttun <ottunmo@mac.com>
2021-04-04 21:10:45 -0500

    Added logic to display current day and time in the the header.

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+// Display the current date and time 
+var currentDay = moment().format('MMMM Do YYYY, h:mm a');
+$('#currentDay').text(currentDay);
+
+
+
+
+
+
+// var date = moment().startOf('hour').fromNow();  
+
+// var time_only = moment().format('2016-01-01 8:00:00 AM', 'hh:mm a');
+
+
+// console.log(date);
+// console.log(time_only);
